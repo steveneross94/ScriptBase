fc3f8711ad59b4c42b41aa85164db4b7b2614abf
steveneross94 <ross.steven@me.com>
2020-08-10 22:44:06 -0400

    articles api works

--- script-base/src/CovidNews/CovidNews.js
+++ script-base/src/CovidNews/CovidNews.js
@@ -1,9 +1,41 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+// import Card from 'react-bootstrap/Card'
+// import Col from 'react-bootstrap/Col'
+// import Row from 'react-bootstrap/Row'
+// import Container from 'react-bootstrap/Container'
 
 function CovidNews() {
+
+    let [covidNews, setCovidNews] = useState(null)
+
+    useEffect(() => {
+        fetch('https://api.covid19api.com/summary')
+            .then(r => r.json())
+            .then(data => setCovidNews(data))
+    }, [])
+
+    console.log(covidNews);
     return (
         <div>
             <h1>Covid News!</h1>
+            <div>
+                Global New Confirmed Case Count: <span className='anim-typewriter'>{covidNews && covidNews.Global.NewConfirmed}</span>
+            </div>
+            <div>
+                Global Total Confirmed Case Count: <span>{covidNews && covidNews.Global.TotalConfirmed}</span>
+            </div>
+            <div>
+                Global New Death Count: <span>{covidNews && covidNews.Global.NewDeaths}</span>
+            </div>
+            <div>
+                Global Total Death Count: <span>{covidNews && covidNews.Global.TotalDeaths}</span>
+            </div>
+            <div>
+                Global New Recovered Count: <span>{covidNews && covidNews.Global.NewRecovered}</span>
+            </div>
+            <div>
+                Global Total Recovered Count: <span>{covidNews && covidNews.Global.TotalRecovered}</span>
+            </div>
         </div>
     )
 }
--- script-base/src/HealthcareNews/HealthcareNews.js
+++ script-base/src/HealthcareNews/HealthcareNews.js
@@ -1,9 +1,41 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import Card from 'react-bootstrap/Card'
+import Col from 'react-bootstrap/Col'
+import Row from 'react-bootstrap/Row'
+import Container from 'react-bootstrap/Container'
+
 
 function HealthcareNews() {
+
+    let [healthNews, setHealthNews] = useState(null)
+
+    useEffect(() => {
+        fetch('http://localhost:3000/api/v1/articles')
+            .then(r => r.json())
+            .then(data => setHealthNews(data.articles))
+    }, [])
+
+
     return (
         <div>
             <h1>Healthcare News!</h1>
+            <Container fluid>
+                {healthNews ? healthNews.map(art =>
+                    <Row>
+                        <Col >
+                            <Card className='news'>
+                                <Card.Img className='article image' src={art.urlToImage} />
+                                <Card.Body>
+                                    <Card.Link href={art.url}><h2>{art.title}</h2></Card.Link>
+                                    <Card.Text>by {art.author}</Card.Text>
+                                    <Card.Text>{art.description}</Card.Text>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                    </Row>
+                ) : 'loading...'}
+            </Container>
+
         </div>
     )
 }
